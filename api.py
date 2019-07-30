10cccc3707cfca32ae331cccdd092316a7bd10ad
blinton2 <bryanlinton2@gmail.com>
2019-07-30 18:40:47 -0500

    Adding more functionality for api.py file.  Includes a function to retrieve movie data from the database

--- api.py
+++ api.py
@@ -1,3 +1,21 @@
 from utilities import get_db,do_query
 
 connection = get_db()
+
+#Movies api function with optional argument for movie id
+def movies(movie_id=-1):
+	if movie_id != -1:
+		movie_query = "SELECT * FROM d_movie where id = 1;"
+		
+	else:
+		movie_query = "SELECT * FROM d_movie;"
+
+	movie_response = do_query(connection,movie_query)
+
+	return movie_response
+
+def crews(crew_id=-1):
+	print("Retrieve crew table")
+
+def movie_ratings(movie_id=-1):
+	print("Retrieve movie ratings")
